0f49aed1a9676f72d6d3b9cc58fe36bcce580858
Isaac Cook <isaac@simpload.com>
2014-03-16 02:00:14 -0500

    Moved the ltc_scrypt extension to be installed by root setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from distutils.core import setup, Extension
+
+
+ltc_scrypt_module = Extension('ltc_scrypt',
+                               sources = ['./ltc_scrypt/scryptmodule.c',
+                                          './ltc_scrypt/scrypt.c'],
+                               include_dirs=['./ltc_scrypt'])
 
 
 setup(name='cryptokit',
@@ -9,5 +16,5 @@ setup(name='cryptokit',
       author='Isaac Cook',
       author_email='isaac@simpload.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      packages=find_packages()
-      )
+      packages=find_packages(),
+      ext_modules=[ltc_scrypt_module])
